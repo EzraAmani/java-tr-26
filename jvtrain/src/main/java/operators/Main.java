a61c6af3940dfa7f6ff33a819af26eb14ea06b1d
EzraAmani <ezraamani02@gmail.com>
2026-01-14 13:21:23 +0300

    looked at operators

---
+++ jvtrain/src/main/java/operators/Main.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 4;
+        int c = (a * b) / 3;
+        System.out.println((a * b / 2) * c);
+    }
+
+}
