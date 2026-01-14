ee5eff0382c3008a3cfef1067541c393b58e592e
EzraAmani <ezraamani02@gmail.com>
2026-01-14 13:01:14 +0300

    primitives done

---
+++ jvtrain/src/main/java/primitives/Main.java
@@ -0,0 +1,24 @@
+package primitives;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Integer types
+        int age = 25;
+        long bigNumber = 123456789L;
+        // Decimal types
+        float price = 19.99f;
+        double precise = 3.1415926535;
+        // Character and boolean
+        char grade = 'A';
+        boolean isJavaFun = true;
+        System.out.println(age);
+        System.out.println(bigNumber);
+        System.out.println(price);
+        System.out.println(precise);
+        System.out.println(grade);
+        System.out.println(isJavaFun);
+
+    }
+
+}
