dbd77b713b0cac8e33f4e7f02cf7a5a383a69235
EzraAmani <ezraamani02@gmail.com>
2026-01-15 08:13:03 +0300

    calculator done

---
+++ jvtrain/src/main/java/calculator/Main.java
@@ -0,0 +1,47 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+        scanner.nextLine();
+        System.out.println("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        scanner.nextLine();
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator");
+                return;
+        }
+
+        System.out.printf("Result: " + num1 + " " + operator + " " + num2 + " = " + result);
+    }
+
+}
---
+++ jvtrain/src/main/java/flow/Main.java
@@ -0,0 +1,20 @@
+package flow;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age please: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        if (age >= 18 && age <= 30) {
+            System.out.println("You are a youth");
+        } else if (age > 30) {
+            System.out.println("You are old");
+        } else {
+            System.out.println("You are a child");
+        }
+    }
+
+}
