6e958589f4b1f86fa26ef1a4224b428f8d4db8d4
EzraAmani <ezraamani02@gmail.com>
2026-01-17 13:05:02 +0300

    working on loops

---
+++ jvtrain/src/main/java/loops/Main.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+            System.out.println("Sum 1-100: " + sum);
+        }
+
+    }
+
+}
---
+++ jvtrain/src/main/java/whileloop/Main.java
@@ -0,0 +1,20 @@
+package whileloop;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String password = "secret123";
+        String userInput = "";
+        Scanner scanner = new Scanner(System.in);
+        while (!userInput.equals(password)) {
+            System.out.print("Enter password: ");
+            userInput = scanner.nextLine();
+            if (!userInput.equals(password)) {
+                System.out.println("Incorrect! Try again.");
+            }
+        }
+        System.out.println("Access granted!");
+
+    }
+}
